a96300a5a9b7cd4ce19e166213bd73f1eabaa367
PublicVar <dev.publicvar@gmail.com>
2017-01-18 12:23:38 +0400

    Installation

---
+++ src/dist/src/app/todo-data.service.spec.js
@@ -0,0 +1,15 @@
+/* tslint:disable:no-unused-variable */
+"use strict";
+var testing_1 = require("@angular/core/testing");
+var todo_data_service_1 = require("./todo-data.service");
+describe('TodoDataService', function () {
+    beforeEach(function () {
+        testing_1.TestBed.configureTestingModule({
+            providers: [todo_data_service_1.TodoDataService]
+        });
+    });
+    it('should ...', testing_1.inject([todo_data_service_1.TodoDataService], function (service) {
+        expect(service).toBeTruthy();
+    }));
+});
+//# sourceMappingURL=todo-data.service.spec.js.map
